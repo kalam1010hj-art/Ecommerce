72b683e0fd314d5c956c63cb1d8c87a3d10c046e
Abdul kalam <kalam1010hj@gmail.com>
2026-09-12 15:55:40 +0530

    sloving image problem

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,21 +1,31 @@
 from rest_framework import serializers
-from .models import Product,ProductImage,Category
+from .models import Product, ProductImage, Category
 
 
 class ProductImageSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+
     class Meta:
         model = ProductImage
         fields = "__all__"
         read_only_fields = ["product"]
+
+    def get_image(self, obj):
+        if not obj.image:
+            return None
+
+        return f"https://res.cloudinary.com/amlkmggs/{obj.image}"
+
+
 class ProductSerializer(serializers.ModelSerializer):
-    images = ProductImageSerializer(read_only = True,many =True)
-    
+    images = ProductImageSerializer(read_only=True, many=True)
+
     class Meta:
         model = Product
         fields = "__all__"
+
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = "__all__"
-        
